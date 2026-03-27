4fd25ad02c6279076215055a563aca34efd7a4c9
Joseph7563 <joseexperts01@gmail.com>
2026-03-27 23:06:11 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function orderNow(product, price) {
-  let phone = "2347044718879"; // Replace with your WhatsApp number
-  let message = `Hello, I want to order ${product} for $${price}`;
-  let url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-  window.open(url, "_blank");
-}
